1323b82b8e81ad01fe3e827253b64666c021b4a6
Hefesto66 <henrique163.cs@gmail.com>
2026-04-09 11:23:35 -0300

    fix(migration): corrige down_revision para b9a922a58e25 e adiciona alembic ao pyproject.toml

--- backend/alembic/versions/a1b2c3d4e5f6_feat_adiciona_campos_justificativa_andon_call.py
+++ backend/alembic/versions/a1b2c3d4e5f6_feat_adiciona_campos_justificativa_andon_call.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, None] = 'f45dafaf98ee'
+down_revision: Union[str, None] = 'b9a922a58e25'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
